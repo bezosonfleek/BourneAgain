4f1b2ad7231fbc79cd7092eb26da0ab8da2dbe58
bezosonfleek <geoffreysakora@gmail.com>
2026-05-09 21:17:55 +0300

    Improved IP script

--- function2.sh
+++ function2.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 showaddress(){
-  execution_time=$(date)
+  #execution_time=$(date)
+  execution_time=$(date "+%Y-%m-%d %H:%M:%S")
   private_ip=$(hostname -I | awk '{print $1}')
   
   if public_ip=$(curl -s --max-time 5 https://ifconfig.me); then
     status="ONLINE"
+    exit_code=0
   else
     public_ip="UNREACHABLE"
     status="OFFLINE"
+    exit_code=1
   fi
 
-  cat << EOF 
---------------------------------------------
+  cat << EOF | tee -a net-audit.log
+------------------------------------------
 Execution time: ${execution_time}
 Network status: ${status}
---------------------------------------------
-Private IP: ${private_ip}
-Public IP: ${public_ip}
---------------------------------------------
+Private IP: ${private_ip:-"N/A"}
+Public IP : ${public_ip}
+------------------------------------------
 EOF
+  return $exit_code
 }
 
 showaddress
