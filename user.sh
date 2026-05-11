7d29a06f93d116c3c36ff59c8cb5be2bd0f3409f
bezosonfleek <geoffreysakora@gmail.com>
2026-05-12 00:03:06 +0300

    added user management script

---
+++ user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "User to be created:" user
+
+if id "$user" &>/dev/null; then
+  echo "Error: User $user already exists."
+  exit 1
+fi
+
+expire_date=$(date -d "+1 year" +%Y-%m-%d)
+
+sudo useradd -m -s /bin/bash -G docker -e "$expire_date" $user
+
+if [ $? -eq 0 ]; then
+  echo "Account for $user created (expires: $expire_date)."
+  sudo passwd $user 
+else
+  echo "Failed to create user."
+  exit 1
+fi
