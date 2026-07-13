d3e3c4c5726a682e9ea8b511b79e234dd7637687
bezosonfleek <geoffreysakora@gmail.com>
2026-07-14 02:45:32 +0300

    feat: filter snap and virtual mounts from disk checks

--- discord-resource-monitor.sh
+++ discord-resource-monitor.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
 threshold=75
 
-df -h | awk 'NR > 1 {sub(/%/, "", $5); print $5, $6}' | while read -r disk_usage mount_point; do
+df -h | awk 'NR > 1 && $6 !~ /^\/snap/ && $6 !~ /^\/usr\/lib\/wsl/ {sub(/%/, "", $5); print $5, $6}' | while read -r disk_usage mount_point; do
     if [ "$disk_usage" -ge "$threshold" ]; then
         echo "Threshold exceeded for $mount_point. Current usage: $disk_usage"
     fi
