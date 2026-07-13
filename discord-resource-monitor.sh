5ca9e7994129c4a71c93eef85acd3606cd155121
bezosonfleek <geoffreysakora@gmail.com>
2026-07-14 02:47:11 +0300

    refactor: load sensitive webhook variable securely from env file

--- discord-resource-monitor.sh
+++ discord-resource-monitor.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-discord_webhook="https://discord.com/api/webhooks/YOUR_WEBHOOK_URL"
+if [ -f "/home/bezos/.env" ]; then
+    source "/home/bezos/.env"
+fi
 
-threshold=75
+threshold=65
 
 df -h | awk 'NR > 1 && $6 !~ /^\/snap/ && $6 !~ /^\/usr\/lib\/wsl/ {sub(/%/, "", $5); print $5, $6}' | while read -r disk_usage mount_point; do
+
     if [ "$disk_usage" -ge "$threshold" ]; then
         echo "Threshold exceeded for $mount_point. Current usage: ${disk_usage}%"
         curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"🚨 **System Alert** 🚨\n**Server:** \$(hostname)\n**Mount Point:** \${mount_point}\n**Disk Usage:** \${disk_usage}%\"}" "$discord_webhook"
