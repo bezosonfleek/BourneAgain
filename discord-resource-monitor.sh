88ab172520cfc396976ddc5b001f21cfed270c2e
bezosonfleek <geoffreysakora@gmail.com>
2026-07-15 00:57:26 +0300

    style: format curl command across multiple lines for better readability

--- discord-resource-monitor.sh
+++ discord-resource-monitor.sh
@@ -10,6 +10,8 @@ df -h | awk 'NR > 1 && $6 !~ /^\/snap/ && $6 !~ /^\/usr\/lib\/wsl/ {sub(/%/, "",
 
     if [ "$disk_usage" -ge "$threshold" ]; then
         echo "Threshold exceeded for $mount_point. Current usage: ${disk_usage}%"
-        curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"🚨 **System Alert** 🚨\n**Server:** \$(hostname)\n**Mount Point:** \${mount_point}\n**Disk Usage:** \${disk_usage}%\"}" "$discord_webhook"
+        curl -H "Content-Type: application/json" \
+             -X POST \
+             -d "{\"content\": \"🚨 **System Alert** 🚨\n**Server:** $(hostname)\n**Mount Point:** ${mount_point}\n**Disk Usage:** ${disk_usage}%\"}" "$discord_webhook"
     fi
 done
