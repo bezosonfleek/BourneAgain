e4279f280838517a8855c3f45ce2c4b65cd7ec21
bezosonfleek <geoffreysakora@gmail.com>
2026-07-23 23:53:24 +0300

    Added error checking

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -eou pipefail
+
 source_dir="$HOME/bash-scripts"
 backup_dest="$HOME/backups"
 date_stamp=$(date +%Y-%m-%d_%H%M)
 file_name="backup_${date_stamp}.tar.gz"
+
+if [[ ! -d "$source_dir" ]]; then
+	echo "Source directory does not exist..." >&2
+	exit 1
+fi
 
 # Create backup directory if it doesn't exist
 [[ ! -d "$backup_dest" ]] && mkdir -p "$backup_dest"
